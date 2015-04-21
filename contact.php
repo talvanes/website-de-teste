6949a4c17ebe04bc6de15e4a077e60df3b14fadd
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2015-04-21 14:03:46 -0300

    Adicionado recurso para inserção e atualização do banco de dados

--- contact.php
+++ contact.php
@@ -56,49 +56,7 @@
         <form name="form1" action="process.php" method="post" autocomplete="off" onsubmit="return validarFormulario();">
             <p>
               <label for="nome" class="required">Nome Completo:</label>
-              <input type="text" list="pessoas" id="nome" class="input input-border" name="nome"  title="Nome e sobrenome" placeholder="Nome completo..." required />
-              <!-- Componente Datalist (será gerado pelo PHP) -->
-              <datalist id="pessoas">
-                <?php 
-                  class DatalistOptions extends RecursiveIteratorIterator {
-                    function __construct($iterator) {
-                      parent::__construct($iterator, self::LEAVES_ONLY);
-                    }
-                    function current() {
-                      return "<option value='".parent::current()."'/>".
-                            parent::current()."</option>\n";
-                    }
-                  }
-                  
-                  $servidor = "localhost";
-                  $usuario = "postgres";
-                  $senha = "N4gy-N1yaz0v";
-                  $banco = "website-teste";
-                  
-                  try {
-                    // Criando a conexão a banco de dados
-                    $conexao = new PDO("pgsql:host=$servidor;port=5432;dbname=$banco", $usuario, $senha);
-                    
-                    // PDO Error no modo de exceção
-                    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    // -- echo "<p>Conectado com sucesso...</p>";
-                    
-                    // obtendo uma lista com os dados de pessoas
-                    $comando = $conexao->prepare("SELECT nome FROM pessoas");
-                    $comando->execute();
-                    
-                    // configurar o array de resultados para associativo
-                    $resultado = $comando->setFetchMode(PDO::FETCH_ASSOC);
-                    foreach (new DatalistOptions(new RecursiveArrayIterator($comando->fetchAll())) as $key => $value) {
-                      echo "$value";
-                    }
-                  } catch (PDOException $ex) {
-                    echo "<p>Falha na conexão: ".$ex->getMessage()."</p>";
-                  }
-                  $conexao = null;
-                ?>
-                <?php  ?>
-              </datalist>
+              <input type="text" id="nome" class="input input-border" name="nome"  title="Nome e sobrenome" placeholder="Nome completo..." required />
             </p>
             <p>
               <label for="contato" class="required">E-Mail para Contato:</label>
--- process.php
+++ process.php
@@ -49,27 +49,89 @@
       
       <section id="content">
         <!-- Exibir os dados já enviados na página process.php -->
-        <h2>Esses são os seus dados:</h2>
+        <h2>Esses foram os seus dados:</h2>
         <table class="round-border">
           <tr class="n3-back-color n3-fore-color">
             <th>Campo:</th>
             <th>Valor:</th>
           </tr>
-          <!--  -->
+          <!-- Mostrar os valores submetidos por formulário (contact.php) -->
           <?php foreach ($_POST as $campo => $valor): ?>
           <tr class="n2-back-color n2-fore-color">
-            <td><?php echo $campo; ?></td>
+            <td style="font-weight: 600; font-style: italic;"><?php echo $campo; ?></td>
             <td><?php echo $valor; ?></td>
           </tr>
-          <!--  -->
           <?php endforeach; ?>
         </table>
-        <p><a href="javascript:history.go(-1);">Voltar...</a></p>
+        <p style="margin-bottom: 10px;">Muito obrigado pela participação.</p>
+        <section style="padding: 12px;" class="n2-back-color n2-fore-color">
         <?php
           /*
            * Código para armazenamento dos dados no banco de dados "website-teste"
            */
+           
+          # 1º Passo: capturar os valores enviados
+          $datahora = date(DATE_W3C);
+          $nome = filter_input(INPUT_POST, 'nome');
+          $email = filter_input(INPUT_POST, 'contato');
+          $datanasc = filter_input(INPUT_POST, 'nasc');
+          $telefone = filter_input(INPUT_POST, 'tel');
+          $cor = filter_input(INPUT_POST, 'cor');
+          $comentario = filter_input(INPUT_POST, 'comentario');
+          
+          # 2º Passo: armazenar os valores no banco de dados
+          // alguns parâmetros do PostgreSql
+          $host = 'localhost';
+          $user = 'postgres';
+          $password = 'N4gy-N1yaz0v';
+          $dbname = 'website-teste';
+          // criando a conexão usando PDO
+          $conexao = new PDO("pgsql:host=$host;port=5432;dbname=$dbname", $user, $password);
+          $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          try {
+            # 3º Passo: preparar alguns comandos de inserção 
+            #           (do tipo "INSERT foo INTO table;")
+            // Comando para a tabela Pessoas
+            $comando_pessoas = $conexao->prepare("INSERT INTO pessoas(nome, datanasc, email, telefone) VALUES (:nome, :datanasc, :email, :telefone)");
+            $comando_pessoas->bindParam(":nome", $nome, PDO::PARAM_STR);
+            $comando_pessoas->bindParam(":datanasc", $datanasc, PDO::PARAM_STR);
+            $comando_pessoas->bindParam(":email", $email, PDO::PARAM_STR);
+            $comando_pessoas->bindParam(":telefone", $telefone, PDO::PARAM_STR);
+            $resultado1 = $comando_pessoas->execute();
+            
+            // Comando para a tabela Comentários
+            $comando_comentarios = $conexao->prepare("INSERT INTO comentarios(datahora, pessoaid, email, cor, comentario) VALUES (:datahora, :pessoaid, :email, :cor, :comentario)");
+            $comando_comentarios->bindParam(":datahora", $datahora, PDO::PARAM_INT);
+            // -- pessoaid (fazer subquery) PDO::PARAM_INT
+            $pessoaid = $conexao->prepare("SELECT id FROM pessoas WHERE nome = :nome");
+            $pessoaid->bindParam(':nome', $nome, PDO::PARAM_STR);
+            if($pessoaid->execute()){
+              # se, após a execução da subquery acima, algum valor tiver sido retornado,
+              # pegue-o e utilize-o
+              $pessoa = $pessoaid->fetch(PDO::FETCH_ASSOC); # transformado em vetor associativo
+              $id = $pessoa['id'];  # um valor obtido a partir do vetor
+            }
+            // --
+            $comando_comentarios->bindParam(":pessoaid", $id, PDO::PARAM_INT);
+            $comando_comentarios->bindParam(":email", $email, PDO::PARAM_STR);
+            $comando_comentarios->bindParam(":cor", $cor, PDO::PARAM_STR);
+            $comando_comentarios->bindParam(":comentario", $comentario, PDO::PARAM_STR);
+            $resultado2 = $comando_comentarios->execute();
+            
+            # 4º Passo: verificar o sucesso da operação
+            if($resultado1 && $resultado2){
+              echo "Dados inseridos com sucesso!";
+            } else {
+              echo "Erro ao inserir os dados";
+            }
+          } catch (PDOException $ex) {
+            echo 'Erro ao criar a conexão: '.$ex->getMessage();
+          }
+          // fechando a programação
+          $conexao = null;
         ?>
+      </section>
+      <p style="margin-top: 20px;"><a href="javascript:history.go(-1);">Voltar...</a></p>
       </section>
       <footer class="n1-back-color">
         <p class="n1-back-color n1-fore-color round-border">Copyright <sup>&copy;</sup> 2015. <strong style="font-style:italic">Fict&iacute;cia SA</strong>. Todos os direitos reservados.</p>
